55f238691566462acbf285415166a6f719417802
pixweb16 <pixweb@protonmail.com>
2021-01-21 22:01:25 +0100

    card.js created not yet styled

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import './App.css';
+import Card from './Card';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header>
-      <h2>Welcome</h2>
-      header
+    <div className="App bg-gray-100">
+      
+
+      <header className="px-7">
+      
+        <h1 className="text-center text-4xl text-gray-500">Welcome</h1>
+        
+        <div className="flex justify-end text-sm">
+          
+          <a href="#" className="text-red-400 hover:text-red-300">
+            <svg className="w-4 h-4 inline mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"></path></svg>
+            Sign-up
+          </a>
+          
+          <a href="#" className="text-red-400 hover:text-red-300 ml-3">
+            <svg className="w-4 h-4 inline mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path></svg>
+            Login
+          </a>
+        
+        </div>
       </header>
-      <main>
-        main
+      
+
+      <main className="px-7">
+        <Card/>
       </main>
+      
+
       <footer>
         footer
       </footer>
---
+++ src/Card.js
@@ -0,0 +1,36 @@
+import card_image from "./imgs/card_image.jpg";
+
+const Card =() => {
+ return(
+
+ 	<div className="bg-white overflow-hidden shadow-md rounded p-2.5">
+ 		
+ 		{/* CARD HEADER */}
+ 		<div className="flex space-between">
+ 			<svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+ 			<span>Profil name</span>
+ 			<div className="flex">
+ 				<span className="text-green-400 text-bold">+15</span>
+ 				<svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14 10h4.764a2 2 0 011.789 2.894l-3.5 7A2 2 0 0115.263 21h-4.017c-.163 0-.326-.02-.485-.06L7 20m7-10V5a2 2 0 00-2-2h-.095c-.5 0-.905.405-.905.905 0 .714-.211 1.412-.608 2.006L7 11v9m7-10h-2M7 20H5a2 2 0 01-2-2v-6a2 2 0 012-2h2.5"></path></svg>
+ 				<svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z"></path></svg>
+ 			</div>
+ 		</div>
+ 		
+ 		{/* CARD IMAGE */} 
+ 		<div>	
+ 			<img src={card_image} alt="image"/>
+ 			<span className="text-2xl">Solde Devise Name</span>
+ 			<hr/>
+ 		</div>
+ 		{/* CARD FOOTER */} 		
+ 		<div className="flex">
+ 			<svg class="w-6 h-6 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+ 			<span>11-01-2021</span>
+ 		</div>
+ 	</div>
+
+
+ 	);
+}
+
+export default Card;
